4b792d5e07df693b0f736726155704d4d28e95e5
Bertrand Caron <bertrand.fx.caron@gmail.com>
2023-04-19 14:51:50 +1000

    Feat: Create DynamoDB user-events table; finish dummy Lambda handler; add deploy script

--- src/Users.ts
+++ src/Users.ts
@@ -1,3 +1,13 @@
-export const handler = (event: unknown): unknown => {
-    return {}
+const DEFAULT_HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': '*',
+    'Access-Control-Allow-Headers': 'Cache-Signature',
+}
+
+export const handler = async (event: unknown): Promise<unknown> => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({data: true}),
+        headers: DEFAULT_HEADERS,
+    }
 }
